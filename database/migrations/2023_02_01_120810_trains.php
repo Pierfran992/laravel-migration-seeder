2468580f8f4e7cb38c3aa6df2f236bad8f5248fc
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-01 15:01:40 +0100

    Create le colonne delle tabelle;

--- database/migrations/2023_02_01_120810_trains.php
+++ database/migrations/2023_02_01_120810_trains.php
@@ -13,7 +13,27 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('trains', function(Blueprint $table){
+            $table -> id();
+
+            $table -> string('azienda', 32);
+            $table -> string('stazione_di_partenza', 64);
+            $table -> string('stazione_di_arrivo', 64);
+
+            $table -> date('data_di_partenza');
+            $table -> time('orario_di_partenza');
+
+            $table -> date('data_di_arrivo');
+            $table -> time('orario_di_arrivo');
+
+            $table -> smallInteger('codice_treno');
+            $table -> smallInteger('numero_carrozze');
+
+            $table -> boolean('in_orario');
+            $table -> boolean('cancellato');
+
+            $table -> timestamps();
+        });
     }
 
     /**
@@ -23,6 +43,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('trains');
     }
 };
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     <h1 class="text-danger">hello world!</h1>
+
+    <p>ciao</p>
 @endsection
