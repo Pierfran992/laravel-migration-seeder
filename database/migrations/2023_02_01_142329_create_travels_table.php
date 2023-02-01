7a275f20ef4e605322e4168e96b1f590e4b99722
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-01 15:38:37 +0100

    Create le colonne della table travels con i relativi modificatori;

--- database/migrations/2023_02_01_142329_create_travels_table.php
+++ database/migrations/2023_02_01_142329_create_travels_table.php
@@ -15,6 +15,21 @@ return new class extends Migration
     {
         Schema::create('travels', function (Blueprint $table) {
             $table -> id();
+
+            $table -> string('codice', 10) -> unique();
+
+            $table -> string('destinazione', 64);
+
+            $table -> date('data_di_partenza');
+            $table -> time('orario_di_partenza');
+
+            $table -> date('data_di_arrivo');
+            $table -> time('orario_di_arrivo');
+
+            $table -> string('mezzo_di_trasporto', 32);
+
+            $table -> integer('prezzo') -> unsigned();
+
             $table -> timestamps();
         });
     }
